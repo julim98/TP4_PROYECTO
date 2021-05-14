659152cfd1e73b7a545c1b789c360b926b21c683
Hassan Mafud <hassanmafud97@gmail.com>
2021-05-14 13:39:11 -0300

    Parte Visual OK

--- Formularios/Main.Designer.cs
+++ Formularios/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnSimular = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.cmbReservas = new System.Windows.Forms.ComboBox();
@@ -38,33 +39,56 @@
             this.txtDesde = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtCantFilas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSimular
             // 
-            this.btnSimular.Location = new System.Drawing.Point(620, 260);
+            this.btnSimular.BackColor = System.Drawing.Color.Transparent;
+            this.btnSimular.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSimular.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSimular.FlatAppearance.BorderSize = 0;
+            this.btnSimular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSimular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSimular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSimular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimular.Image = ((System.Drawing.Image)(resources.GetObject("btnSimular.Image")));
+            this.btnSimular.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSimular.Location = new System.Drawing.Point(209, 310);
             this.btnSimular.Name = "btnSimular";
-            this.btnSimular.Size = new System.Drawing.Size(75, 23);
+            this.btnSimular.Size = new System.Drawing.Size(125, 60);
             this.btnSimular.TabIndex = 0;
             this.btnSimular.Text = "Simular";
-            this.btnSimular.UseVisualStyleBackColor = true;
+            this.btnSimular.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSimular.UseVisualStyleBackColor = false;
             this.btnSimular.Click += new System.EventHandler(this.btnSimular_Click);
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(514, 260);
+            this.btnSalir.BackColor = System.Drawing.Color.Transparent;
+            this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSalir.FlatAppearance.BorderColor = System.Drawing.Color.Maroon;
+            this.btnSalir.FlatAppearance.BorderSize = 0;
+            this.btnSalir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Image = ((System.Drawing.Image)(resources.GetObject("btnSalir.Image")));
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(427, 401);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(104, 32);
             this.btnSalir.TabIndex = 1;
             this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSalir.UseVisualStyleBackColor = false;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // cmbReservas
             // 
             this.cmbReservas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbReservas.FormattingEnabled = true;
-            this.cmbReservas.Location = new System.Drawing.Point(134, 130);
+            this.cmbReservas.Location = new System.Drawing.Point(186, 133);
             this.cmbReservas.Name = "cmbReservas";
             this.cmbReservas.Size = new System.Drawing.Size(121, 21);
             this.cmbReservas.TabIndex = 2;
@@ -72,65 +96,89 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(25, 133);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.Size = new System.Drawing.Size(72, 18);
             this.label1.TabIndex = 3;
             this.label1.Text = "Reservas";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.InfoText;
             this.label2.Location = new System.Drawing.Point(25, 107);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.Size = new System.Drawing.Size(144, 18);
             this.label2.TabIndex = 4;
             this.label2.Text = "Cantidad de vuelos";
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(134, 104);
+            this.txtCantidad.Location = new System.Drawing.Point(186, 107);
             this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.Size = new System.Drawing.Size(121, 20);
             this.txtCantidad.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold);
             this.label3.Location = new System.Drawing.Point(25, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.Size = new System.Drawing.Size(52, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "Desde";
             // 
             // txtDesde
             // 
-            this.txtDesde.Location = new System.Drawing.Point(134, 157);
+            this.txtDesde.Location = new System.Drawing.Point(186, 160);
             this.txtDesde.Name = "txtDesde";
-            this.txtDesde.Size = new System.Drawing.Size(100, 20);
+            this.txtDesde.Size = new System.Drawing.Size(123, 20);
             this.txtDesde.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold);
             this.label4.Location = new System.Drawing.Point(25, 186);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.Size = new System.Drawing.Size(115, 18);
             this.label4.TabIndex = 4;
             this.label4.Text = "Filas a mostrar";
             // 
             // txtCantFilas
             // 
-            this.txtCantFilas.Location = new System.Drawing.Point(134, 183);
+            this.txtCantFilas.Location = new System.Drawing.Point(186, 184);
             this.txtCantFilas.Name = "txtCantFilas";
-            this.txtCantFilas.Size = new System.Drawing.Size(100, 20);
+            this.txtCantFilas.Size = new System.Drawing.Size(123, 20);
             this.txtCantFilas.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 27.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(38, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(296, 50);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Pasajeros Aereos";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(707, 295);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(543, 461);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.txtCantFilas);
             this.Controls.Add(this.txtDesde);
             this.Controls.Add(this.label4);
@@ -142,7 +190,8 @@
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnSimular);
             this.Name = "Main";
-            this.Text = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T.P Nº4 Pasajes Aereos";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,5 +209,6 @@
         private System.Windows.Forms.TextBox txtDesde;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtCantFilas;
+        private System.Windows.Forms.Label label5;
     }
 }
--- Formularios/Resultado.Designer.cs
+++ Formularios/Resultado.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Resultado));
             this.dgvVuelos = new System.Windows.Forms.DataGridView();
             this.btnCerrar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -42,38 +43,50 @@
             this.dgvVuelos.AllowUserToResizeColumns = false;
             this.dgvVuelos.AllowUserToResizeRows = false;
             this.dgvVuelos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvVuelos.BackgroundColor = System.Drawing.Color.LightCyan;
             this.dgvVuelos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvVuelos.Location = new System.Drawing.Point(12, 99);
+            this.dgvVuelos.Location = new System.Drawing.Point(12, 108);
             this.dgvVuelos.Name = "dgvVuelos";
             this.dgvVuelos.ReadOnly = true;
             this.dgvVuelos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
             this.dgvVuelos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            this.dgvVuelos.Size = new System.Drawing.Size(1014, 340);
+            this.dgvVuelos.Size = new System.Drawing.Size(814, 329);
             this.dgvVuelos.TabIndex = 0;
             // 
             // btnCerrar
             // 
-            this.btnCerrar.Location = new System.Drawing.Point(951, 445);
+            this.btnCerrar.BackColor = System.Drawing.Color.Transparent;
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrar.Location = new System.Drawing.Point(773, 548);
             this.btnCerrar.Name = "btnCerrar";
-            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.Size = new System.Drawing.Size(111, 30);
             this.btnCerrar.TabIndex = 1;
             this.btnCerrar.Text = "Cerrar";
-            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCerrar.UseVisualStyleBackColor = false;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(55, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(79, 18);
             this.label1.TabIndex = 2;
             this.label1.Text = "Promedio";
             // 
             // txtPromedio
             // 
             this.txtPromedio.Enabled = false;
-            this.txtPromedio.Location = new System.Drawing.Point(140, 55);
+            this.txtPromedio.Location = new System.Drawing.Point(155, 56);
             this.txtPromedio.Name = "txtPromedio";
             this.txtPromedio.Size = new System.Drawing.Size(100, 20);
             this.txtPromedio.TabIndex = 3;
@@ -82,12 +95,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1038, 480);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1003, 618);
             this.Controls.Add(this.txtPromedio);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.dgvVuelos);
             this.Name = "Resultado";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Resultado";
             ((System.ComponentModel.ISupportInitialize)(this.dgvVuelos)).EndInit();
             this.ResumeLayout(false);
